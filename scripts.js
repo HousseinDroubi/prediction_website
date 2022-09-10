515f90c4f1f5fff48d5aadc13c861e54354aefa1
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-09-10 15:29:30 +0300

    get dog image from json

--- scripts.js
+++ scripts.js
@@ -5,15 +5,46 @@ const fetch_age=document.getElementById("fetch_age");
 const fetch_nationality=document.getElementById("fetch_nationality");
 const input_name=document.getElementById("input_name");
 
+
+fetch('https://dog.ceo/api/breeds/image/random')
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(Object.values(data)[0])
+    let image=Object.values(data)[0];
+    console.log(image)
+  });
+
 let removeError=(e)=>{
     input_name.placeholder='Enter your name';
     input_name.classList.remove('wrong-input');
 }
-let fetchdata = (e) =>{
-    if(input_name.innerText.trim.length==0){
+
+let catchError = (e)=>{
+    if(input_name.value.length==0){
         input_name.placeholder='Required !!';
         input_name.classList.add('wrong-input');
     }
+    else{
+    //     removeError();
+    //     // console.log('sadsad')
+    //     fetch("https://dog.ceo/api/breeds/image/random")
+    //     .then(response => {
+    //     // response=response.json();
+    //     console.log(response.body)
+    // })
+    // .catch(error => {
+    //     // handle the error
+    // });  
+    //     // .then((response) => response.json())
+    //     // .then((data) =>console.log(Object.values(data)[0]))
+    //     // .then(re=>{
+    //     //      image.innerHTML="<img src='${Object.values(re)[0]}'/>"
+    //     //  })
+        
+    fetch('https://dog.ceo/api/breeds/image/random')
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+    }
 }
-btn_fetch.addEventListener('click',fetchdata);
+btn_fetch.addEventListener('click',catchError);
 input_name.addEventListener('click',removeError);
