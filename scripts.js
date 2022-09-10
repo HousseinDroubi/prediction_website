c6354fbaeebd37706d0732368cbdd40a4fe20d4d
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-09-10 18:06:23 +0300

    Finish all requirements

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ const fetch_gender=document.getElementById("fetch_gender");
 const fetch_age=document.getElementById("fetch_age");
 const fetch_nationality=document.getElementById("fetch_nationality");
 const input_name=document.getElementById("input_name");
-
+const fetch_name=document.getElementById("fetch_name");
 // Get the json request from url and fill image
 fetch('https://dog.ceo/api/breeds/image/random')
 .then((response) => response.json())
@@ -12,7 +12,7 @@ fetch('https://dog.ceo/api/breeds/image/random')
     let image_content=Object.values(data)[0];
     dog_image.innerHTML='<img src= "'+image_content+'"/>';
   });
-// Return input text to inital case
+// Return input text to its inital state
 let removeError=(e)=>{
     input_name.placeholder='Enter your name';
     input_name.classList.remove('wrong-input');
@@ -25,7 +25,7 @@ let catchError = (e)=>{
     }
     else{
         removeError();
-        // Fetch gender
+        // Fetch age
         let gender_url='https://api.genderize.io/?name=';
         gender_url=gender_url.concat(input_name.value)
         // console.log(gender_url)
@@ -33,15 +33,48 @@ let catchError = (e)=>{
           .then((response) => response.json())
           .then((data) => {
           let fetched_gender=Object.values(data)[1];
-        //   fetch_gender.innerText=fetched_gender;
-        const div = document.createElement('div');
-        div.innerText=fetched_gender;
-        fetch_gender.appendChild(div);
+        const div_gender = document.createElement('div');
+        div_gender.innerText=fetched_gender;
+        fetch_gender.appendChild(div_gender);})
 
-          
-
-  })
+  // Fetch age
+  let age_url='https://api.agify.io/?name=';
+  age_url=age_url.concat(input_name.value)
+  fetch(age_url)
+    .then((response) => response.json())
+    .then((data) => {
+    let fetched_age=Object.values(data)[1];
+    const div_age = document.createElement('div');
+    div_age.innerText=fetched_age;
+    fetch_age.appendChild(div_age);})
+// Fetch Nationality
+  let Nationality_url='https://api.nationalize.io/?name=';
+  Nationality_url=Nationality_url.concat(input_name.value)
+  fetch(Nationality_url)
+    .then((response) => response.json())
+    .then((data) => {
+    let fetched_nationality=Object.values(data)[1];
+    let nationalities='';
+    for (let i=0;i<fetched_nationality.length;i++){
+        let list1=Object.values(fetched_nationality)[i];
+        let list2 = Object.values(list1)[0];
+        console.log(list2)
+        nationalities=nationalities.concat(list2)+',';
     }
+    const div_nationality= document.createElement('div');
+    div_nationality.innerText=nationalities;
+    fetch_nationality.appendChild(div_nationality);
+
+    const div_name=document.createElement('div');
+    div_name.innerText=input_name.value;
+
+    fetch_name.appendChild(div_name)
+    
+    
+})
+    }
+
+    
 }
 btn_fetch.addEventListener('click',catchError);
 input_name.addEventListener('click',removeError);
